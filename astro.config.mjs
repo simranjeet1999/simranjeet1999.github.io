a53e0a39b15531dd8f256a69cb4f84b14a42af7e
Ssandhu <simransandhu.sandhu7@gmail.com>
2026-01-08 17:53:37 -0600

    for-deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio_simranjeet.github.io',
+  base: '/simranjeet1999',
   vite: {
     plugins: [tailwindcss()],
   },
